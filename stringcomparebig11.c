46f7c423cc36ab5c083c27336fd41361b1f5a13c
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2025-10-15 18:50:22 +0600

    new2

---
+++ stringcomparebig11.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    char st[122],sd[1221];
+    
+    scanf("%s %s",&st, &sd);
+
+    int value=strcmp(st,sd);
+
+    if(value>0)
+    {
+        printf("st is bigger");
+    }
+    
+     else if(value<0)
+     {
+        printf("st is smaller");
+     }
+     else if(value==0)
+
+     {
+        printf("equal");
+     }
+}

097b9a0693484f8ebb9f79f2ffba7f2c3d68324d
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2025-10-16 22:22:33 +0600

    newwww

--- string_cat10.c
+++ string_cat10.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 int main()
+
 {
     char st[233],sd[3445];
     scanf("%s %s",&st ,&sd);
     int length1 =strlen(st);
     int length2=strlen(sd);
 for(int i=0;i<=length2;i++)
+
+
 {
     st[i+length1]=sd[i];
 }
+
 printf("%s %s",st ,sd);
 
 

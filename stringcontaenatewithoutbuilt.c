e56c849246980214b2d6ae68a0c10472b4bc79c2
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2026-01-12 17:03:33 +0600

    4

--- stringcontaenatewithoutbuilt.c
+++ stringcontaenatewithoutbuilt.c
@@ -3,19 +3,22 @@
 int main()
 {
     char st[233],sd[3445];
-    scanf("%s %s",st ,sd);
-    
-                                  //scanf("%s",&st)  space doesnot count;
-     int count=0,count2=0;
+gets(st);
+gets(sd);
+                                 //scanf("%s",&st)  space doesnot count;
+int count=0,count2=0;
 for(int i=0;st[i]!='\0';i++)
 {
     count++;
 }
+
+int length=count;
+
 for(int i=0;sd[i]!='\0';i++)
 {
     count2++;
 }
-int length=count;
+
 int length2=count2;
 
 for(int i=length;i<=length+length2;i++)
